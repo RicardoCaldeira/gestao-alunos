86f25063a242f4efb0e371921073826c935f07c9
Ricardo Caldeira <ricardocaldeira.zetta@ufla.br>
2021-09-13 22:33:54 -0300

    isolando Compoente Aluno de Alunos

--- frontend/src/components/Alunos.jsx
+++ frontend/src/components/Alunos.jsx
@@ -1,8 +1,4 @@
-import { useState, useEffect } from "react";
-import {
-  AiOutlineEdit as EditIcon,
-  AiOutlineDelete as DeleteIcon,
-} from 'react-icons/ai'
+import Aluno from "./Aluno";
 
 export default function Alunos({
   onDelete = null,
@@ -10,18 +6,15 @@ export default function Alunos({
   alunos = []
 }) {
 
-  let currentAluno = null;
-  
   function handleEditIconClick(aluno) {
     if (onEdit) {
-      currentAluno = aluno;
-      onEdit(currentAluno);
+      onEdit(aluno);
     }
   }
 
-  function handleDeleteIconClick() {
+  function handleDeleteIconClick(idAluno) {
     if (onDelete) {
-      onDelete(currentAluno.id);
+        onDelete(idAluno);
     }
   }
   return (
@@ -31,65 +24,13 @@ export default function Alunos({
     >
       <ul className="flex flex-row flex-wrap">
         {alunos.map((aluno, index) => {
-          const imgPerfil = aluno.id + ".jpg";
           return (
             <div>
               <li
                 className="shadow-lg p-4 m-2 border flex flex-col items-center justify-center"
                 key={aluno.id}
               >
-                <div className="flex flex-row items-center justify-between space-x-4">
-                  <img
-                    src={`../../../imagens/${imgPerfil}`}
-                    alt={aluno.nome}
-                    style={{
-                      borderRadius: "50%",
-                      height: "100px",
-                      width: "100px",
-                    }}
-                    className="rounded-full"
-                  />
-
-                </div>
-
-                <div>
-                  <h3 className="text-center font-bold pb-2">{aluno.nome}</h3>
-                  <hr />
-                  <div className="mt-2">
-                    <span className="font-semibold">Rua: </span>
-                    <span className="">
-                      {aluno.endereco.logradouro}, {aluno.endereco.numero}
-                    </span>
-                    <br />
-                    <span className="font-semibold">Bairro: </span>
-                    <span className="">{aluno.endereco.bairro}</span>
-                    <br />
-                    <span className="font-semibold">Cidade: </span>
-                    <span className="">{aluno.endereco.cidade}</span>
-                    <br />
-                    <span className="font-semibold">Estado: </span>
-                    <span className="">{aluno.endereco.estado}</span>
-                    {aluno.endereco.complemento !== "" && (
-                      <>
-                      <br/>
-                        <span className="font-semibold">Complemento: </span>
-                        <span className="">{aluno.endereco.complemento}</span>
-                      </>
-                    )}
-                  </div>
-                </div>
-                <div className="mt-4 flex flex-row items-center justify-end space-x-4">
-                  <EditIcon
-                    onClick={handleEditIconClick(aluno)}
-                    className="cursor-pointer"
-                    size={24}
-                  />
-                  <DeleteIcon
-                    onClick={handleDeleteIconClick}
-                    className="cursor-pointer"
-                    size={24}
-                  />
-              </div>
+                <Aluno aluno={aluno} onEdit={handleEditIconClick} onDelete={handleDeleteIconClick} />
               </li>
             </div>
           );
