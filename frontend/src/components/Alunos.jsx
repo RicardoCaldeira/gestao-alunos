d58c198a0d5a0c5f1aa3f4479f65f8245cc8e662
Ricardo Caldeira <ricardocaldeira.zetta@ufla.br>
2021-09-16 09:26:40 -0300

    Key sendo passada ao lugar correto

--- frontend/src/components/Alunos.jsx
+++ frontend/src/components/Alunos.jsx
@@ -25,10 +25,8 @@ export default function Alunos({
       <ul className="flex flex-row flex-wrap items-center justify-center">
         {alunos.map((aluno, index) => {
           return (
-            <div className="shadow-lg p-3 m-2 border flex flex-row items-center justify-center" style={{ width: "350px" }}>
-              <li
-                key={aluno.id}
-              >
+            <div key={aluno.id} className="shadow-lg p-3 m-2 border flex flex-row items-center justify-center" style={{ width: "350px" }}>
+              <li>
                 <Aluno aluno={aluno} onEdit={handleEditIconClick} onDelete={handleDeleteIconClick} />
               </li>
             </div>
